7bb8be1c2464838db488419fb2769f73045d2d03
Concept <conceptcodes@gmail.com>
2022-01-16 20:52:38 -0500

    init

--- database/models/index.js
+++ database/models/index.js
@@ -8,12 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config');
 const db = {};
 
-const sequelize = new Sequelize(config.development.url, {
+const sequelize = new Sequelize(config.production.url, {
   dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false
-    // }
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 }
 );
